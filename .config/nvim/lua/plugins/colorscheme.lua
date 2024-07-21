8836cd68bcbc310b6024473e53af5aeb1bba1fbd
Farrizal Alchudry Mutaqien <farrizalchudry@gmail.com>
2024-07-21 08:58:36 +0700

    chore: change colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,19 @@
+---@diagnostic disable: undefined-global
 return {
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_foreground = "original"
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_enable_bold = true
+      vim.g.gruvbox_material_transparent_background = 0
+      vim.cmd("set background=light")
+    end,
+  },
   { "rebelot/kanagawa.nvim", opts = {
     transparent = true,
   } },
@@ -13,7 +27,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_dark_default",
+      colorscheme = "gruvbox-material",
     },
   },
 }
